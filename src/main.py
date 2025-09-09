c0118aa0e21a2df84efe6a5272ce33b2351776fb
snoopytime68 <144932681+snoopytime68@users.noreply.github.com>
2025-09-09 22:19:29 +0000

    Init commit.

---
+++ src/main.py
@@ -0,0 +1,31 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       codespace                                                    #
+# 	Created:      9/9/2025, 9:59:41 PM                                         #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+brain = Brain()
+
+def autonomous():
+    brain.screen.clear_screen()
+    brain.screen.print("autonomous code")
+    # place automonous code here
+
+def user_control():
+    brain.screen.clear_screen()
+    brain.screen.print("driver control")
+    # place driver control in this while loop
+    while True:
+        wait(20, MSEC)
+
+# create competition instance
+comp = Competition(user_control, autonomous)
+
+# actions to do when the program starts
+brain.screen.clear_screen()
