d67642b2204355869daedf6811810ab6a74b895d
Jbugness2 <Despainjonah@gmail.com>
2025-09-11 23:11:08 +0000

    Reorg code for clarity.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # ---------------------------------------------------------------------------- #
 #                                                                              #
 # 	Module:       main.py                                                      #
-# 	Author:       Jonah and Liam                                                    #
+# 	Author:       Jonah Despain and Liam Fryar                                 #
 # 	Created:      9/9/2025, 9:59:41 PM                                         #
 # 	Description:  V5 project                                                   #
 #                                                                              #
@@ -9,6 +9,20 @@
 
 # Library imports
 from vex import *
+
+# Create the left Motors and group them under the
+# MotorGroup "left_motors".
+left_motor_a = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+left_motor_b = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+left_motors = MotorGroup(left_motor_a, left_motor_b)
+# Create the right Motors and group them under the
+# MotorGroup "right_motors".
+right_motor_a = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)
+right_motor_b = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
+right_motors = MotorGroup(right_motor_a, right_motor_b)
+# Construct a 4-Motor Drivetrain "drivetrain" with the
+# DriveTrain class.
+drivetrain = DriveTrain(left_motors, right_motors, 319.19, 295, 40, MM, 1)
 
 brain = Brain()
 optic = optic(ports.port1)
@@ -27,30 +41,11 @@ def user_control():
     while True:
         wait(20, MSEC)
 
-        # Create the left Motors and group them under the
-        # MotorGroup "left_motors".
-        left_motor_a = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-        left_motor_b = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
-        left_motors = MotorGroup(left_motor_a, left_motor_b)
-        # Create the right Motors and group them under the
-        # MotorGroup "right_motors".
-        right_motor_a = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)
-        right_motor_b = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
-        right_motors = MotorGroup(right_motor_a, right_motor_b)
-        # Construct a 4-Motor Drivetrain "drivetrain" with the
-        # DriveTrain class.
-        drivetrain = DriveTrain(left_motors, right_motors, 319.19, 295, 40, MM, 1)
-
 def buttonDown(button_pressed) 
-    motor1.temperature(units) 
-    motor2.temperature(units) 
-    motor3.temperature(units) 
-    motor4.temperature(units) 
-
-        
-
-
-
+    motor1.temperature() 
+    motor2.temperature() 
+    motor3.temperature() 
+    motor4.temperature() 
 
 # create competition instance
 comp = Competition(user_control, autonomous)
