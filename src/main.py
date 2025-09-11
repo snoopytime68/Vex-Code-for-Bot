5fdff6083fc9f1f9b421b9047d0e74c3aef5feea
snoopytime68 <144932681+snoopytime68@users.noreply.github.com>
2025-09-11 22:11:12 +0000

    Added Drive Train

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@
 from vex import *
 
 brain = Brain()
+optic = optic()
+controller = controller()
 
 def autonomous():
     brain.screen.clear_screen()
@@ -23,6 +25,23 @@ def user_control():
     # place driver control in this while loop
     while True:
         wait(20, MSEC)
+        
+        # Create the left Motors and group them under the
+        # MotorGroup "left_motors".
+        left_motor_a = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+        left_motor_b = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+        left_motors = MotorGroup(left_motor_a, left_motor_b)
+        # Create the right Motors and group them under the
+        # MotorGroup "right_motors".
+        right_motor_a = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)
+        right_motor_b = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
+        right_motors = MotorGroup(right_motor_a, right_motor_b)
+        # Construct a 4-Motor Drivetrain "drivetrain" with the
+        # DriveTrain class.
+        drivetrain = DriveTrain(left_motors, right_motors, 319.19, 295, 40, MM, 1)
+
+
+
 
 # create competition instance
 comp = Competition(user_control, autonomous)
