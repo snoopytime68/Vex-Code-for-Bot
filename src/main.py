c22dc33eeb7316a77aefb2bf4cbf764676a74712
snoopytime68 <144932681+snoopytime68@users.noreply.github.com>
2025-09-23 23:07:49 +0000

    Fixed Drive Train and removed undefined variables

--- src/main.py
+++ src/main.py
@@ -8,10 +8,17 @@ brain=Brain()
 
 # Robot configuration code
 # Drive Train
-motor_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-motor_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
-motor_3 = Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
-motor_4 = Motor(Ports.PORT4, GearSetting.RATIO_18_1, False)
+controller_1 = Controller(PRIMARY)
+left_motor_a = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+left_motor_b = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+left_drive_smart = MotorGroup(left_motor_a, left_motor_b)
+right_motor_a = Motor(Ports.PORT3, GearSetting.RATIO_18_1, True)
+right_motor_b = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
+right_drive_smart = MotorGroup(right_motor_a, right_motor_b)
+drivetrain = DriveTrain(left_drive_smart, right_drive_smart, 319.19, 295, 40, MM, 1)
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
 
 # Intake
 motor_5 = Motor(Ports.PORT5, GearSetting.RATIO_18_1, False)
@@ -38,6 +45,64 @@ def initializeRandomSeed():
 # Set random seed 
 initializeRandomSeed()
 
+# define variables used for controlling motors based on controller inputs
+drivetrain_l_needs_to_be_stopped_controller_1 = False
+drivetrain_r_needs_to_be_stopped_controller_1 = False
+
+# define a task that will handle monitoring inputs from controller_1
+def rc_auto_loop_function_controller_1():
+    global drivetrain_l_needs_to_be_stopped_controller_1, drivetrain_r_needs_to_be_stopped_controller_1, remote_control_code_enabled
+    # process the controller input every 20 milliseconds
+    # update the motors based on the input values
+    while True:
+        if remote_control_code_enabled:
+            
+            # calculate the drivetrain motor velocities from the controller joystick axies
+            # left = axis2 + axis4
+            # right = axis2 - axis4
+            drivetrain_left_side_speed = controller_1.axis2.position() + controller_1.axis4.position()
+            drivetrain_right_side_speed = controller_1.axis2.position() - controller_1.axis4.position()
+            
+            # check if the value is inside of the deadband range
+            if drivetrain_left_side_speed < 5 and drivetrain_left_side_speed > -5:
+                # check if the left motor has already been stopped
+                if drivetrain_l_needs_to_be_stopped_controller_1:
+                    # stop the left drive motor
+                    left_drive_smart.stop()
+                    # tell the code that the left motor has been stopped
+                    drivetrain_l_needs_to_be_stopped_controller_1 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the left motor next
+                # time the input is in the deadband range
+                drivetrain_l_needs_to_be_stopped_controller_1 = True
+            # check if the value is inside of the deadband range
+            if drivetrain_right_side_speed < 5 and drivetrain_right_side_speed > -5:
+                # check if the right motor has already been stopped
+                if drivetrain_r_needs_to_be_stopped_controller_1:
+                    # stop the right drive motor
+                    right_drive_smart.stop()
+                    # tell the code that the right motor has been stopped
+                    drivetrain_r_needs_to_be_stopped_controller_1 = False
+            else:
+                # reset the toggle so that the deadband code knows to stop the right motor next
+                # time the input is in the deadband range
+                drivetrain_r_needs_to_be_stopped_controller_1 = True
+            
+            # only tell the left drive motor to spin if the values are not in the deadband range
+            if drivetrain_l_needs_to_be_stopped_controller_1:
+                left_drive_smart.set_velocity(drivetrain_left_side_speed, PERCENT)
+                left_drive_smart.spin(FORWARD)
+            # only tell the right drive motor to spin if the values are not in the deadband range
+            if drivetrain_r_needs_to_be_stopped_controller_1:
+                right_drive_smart.set_velocity(drivetrain_right_side_speed, PERCENT)
+                right_drive_smart.spin(FORWARD)
+        # wait before repeating the process
+        wait(20, MSEC)
+
+# define variable for remote controller enable/disable
+remote_control_code_enabled = True
+
+rc_auto_loop_thread_controller_1 = Thread(rc_auto_loop_function_controller_1)
 
 # Color to String Helper
 def convert_color_to_string(col):
@@ -133,11 +198,6 @@ def user_control():
             motor_7.stop()                
             pass
 
-def buttonDown(button_pressed) 
-    motor_1.temperature() 
-    motor_2.temperature() 
-    motor_3.temperature() 
-    motor_4.temperature()
 
 # create competition instance
 comp = Competition(user_control, autonomous)
